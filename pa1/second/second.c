187015a84f89e8ca9bef0176eee2ad2f3f87b955
Isabella Pham <isabella.aivy.pham@gmail.com>
2019-09-15 22:19:15 -0400

    testing nodes

--- pa1/second/second.c
+++ pa1/second/second.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node;
-struct node* new(int n, struct node head);
-void print(struct node node);
+typedef struct node{
+  int data;
+  struct node * next;
+}node;
+void print(node * head){
+  while(head != NULL){
+    printf("%d ", head->data);
+    head = head->next;
+  }
+}
 int main(int argc, char** argv){
   /*FILE * f = fopen(argv[1], "r");
   if(ftell(f) == 0){
     printf("%d", 0);
     return 0;
   }*/
-  int total = 2; //total number of nodes
+  int total = 0; //total number of nodes
   //struct node head;
   char operation;
   int n;
@@ -28,33 +35,27 @@ int main(int argc, char** argv){
       total--;
     }
   }*/
-  struct node head = new(1, head);
-  head->next = new(2, head->next);
+  node * head = (node*)malloc(sizeof(node)); //allocating space to node in memory
+  head->data = 1;
+  head->next = NULL;
+  node * ptr = (node*)malloc(sizeof(node));
+  head->next = ptr;
+  ptr->data = 2;
+  ptr->next=NULL;
   printf("%d\n", total);
   print(head);
   //fclose(f);
   return 0;
 }
-typedef struct node{
-  int n;
-  struct node *next;
-}
-struct node new(int n, struct node head){
-  struct node new = malloc(sizeof(struct node));
+/*struct node new(int n, struct node *head){
+  struct node *new = malloc(sizeof(struct node));
   new->n=n;
   new->next=head;
-  return new;
-}
+  return *new;
+}*/
 /*node* insert(int data; node* head){
 
 }
 node* delete(int data; node* head){
 
 }*/
-void print(struct node node){
-  while(node != null){
-    printf("%d ", (node->n));
-    node = node -> next;
-  }
-
-}
