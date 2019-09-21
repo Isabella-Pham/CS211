d81fc44e5b10e4cb46defc35e9673f2b46c1f4ba
Isabella Pham <isabella.aivy.pham@gmail.com>
2019-09-21 15:23:40 -0400

    finished eighth

--- pa1/eighth/eighth.c
+++ pa1/eighth/eighth.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node{
+  int data;
+  struct node* left;
+  struct node* right;
+}node;
+node* insert(node*root, int data, int height){
+  node* add = (node*)malloc(sizeof(node));
+  add->data=data;
+  add->left=NULL;
+  add->right=NULL;
+  if(root == NULL){
+    printf("inserted %d\n",height);
+    return add;
+  }
+  if(data < root->data){
+      root->left = insert(root->left,data,height+1);
+  }else if(data > root->data){
+    root->right = insert(root->right,data,height+1);
+  }else{
+    printf("duplicate\n");
+    return root;
+  }
+}
+void search(node*root, int data, int height){
+  if(root == NULL){
+    printf("absent\n");
+    return;
+  }
+  if(root->data == data){
+    printf("present %d\n", height);
+    return;
+  }
+  if(data < root->data) return search(root->left,data,height+1);
+  if(data > root->data) return search(root->right,data,height+1);
+}
 int main(int argc, char const *argv[]) {
-  /* code */
+  FILE * f = fopen(argv[1], "r");
+  if(f==NULL){
+    printf("error");
+    return 0;
+  }
+  node * root = (node*)malloc(sizeof(node)); //allocating space to node in memory
+  root = NULL;
+  char operation;
+  int data;
+  while(fscanf(f,"%c %d\n", &operation, &data) != EOF){
+    //printf("%d\n",data);
+    if(operation == 'i'){ //insert
+      root = insert(root, data, 1);
+    }else{ //search
+      search(root,data,1);
+    }
+  }
+  fclose(f);
   return 0;
 }
