1d4db28e25f72eba786923fc69d57469b0f3c57f
Isabella Pham <isabella.aivy.pham@gmail.com>
2019-09-21 13:32:00 -0400

    finished seventh

--- pa1/seventh/seventh.c
+++ pa1/seventh/seventh.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int getLength(char * str){
+  int length = 0;
+  while(str[length] != '\0'){
+    length++;
+  }
+  return length;
+}
 int main(int argc, char const *argv[]) {
-  /* code */
+  if(argv[1] == NULL){
+    printf("");
+    return 0;
+  }
+  char * str;
+  for(int i = 1; i < argc; i++){
+    str = (char *)argv[i];
+    int length = getLength(str);
+    char end = str[length-1];
+    printf("%c",end);
+  }
   return 0;
 }
